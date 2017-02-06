64d8b0551bfb86a1ed18d07d25f4d36ee4655fba
Tony Bowden <tony@tmtm.com>
2017-02-06 19:31:02 +0000

    Scrape in 50s rather than 100s

--- scraper.rb
+++ scraper.rb
@@ -9,7 +9,7 @@ require 'scraperwiki'
 require_relative 'lib/politician'
 
 house = EveryPolitician::Index.new.country('Germany').lower_house
-house.popolo.persons.map(&:wikidata).compact.each_slice(100) do |wanted|
+house.popolo.persons.map(&:wikidata).compact.each_slice(50) do |wanted|
   data = Wikisnakker::Politician.find(wanted).flat_map(&:positions).compact
   ScraperWiki.save_sqlite(%i(id position start_date), data)
 end
